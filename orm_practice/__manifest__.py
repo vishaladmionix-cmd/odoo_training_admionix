0b89d25107dded89c1c9c396dc7c9ea92f824b1f
admionix <vishal.admionix@gmail.com>
2026-03-02 17:20:39 +0530

    update

---
+++ orm_practice/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'ORM Practice',
+    'version': '19.0',
+    'summary' : 'orm practice',
+    'author' : 'ADmionix',
+    'category':'Trainig',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/student_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ orm_practice/models/student.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+class Student(models.Model):
+    _name = 'orm.practice.student'
+    _description = 'ORM Practice Student'
+
+    name = fields.Char(string='Name',required=True)
+    age = fields.Integer(string='Age',required=True)
+    email = fields.Char()
+    active = fields.Boolean(default=True)
+
